b74ed76058ad97a3001c03f18f0f1d0d3d48fef2
stamang <stamang.mrbhutan90@gmail.com>
2025-08-07 21:21:34 +0800

    feat: Add hero section for landing page

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,7 +25,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased bg-gray-950`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased dark:bg-gray-950`}
       >
         {children}
       </body>
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,38 @@
 import React from "react";
 
-const page = () => {
-  return <div>page</div>;
+const Hero = () => {
+  return (
+    <section className="relative min-h-screen bg-[url(/assets/hero-2.png)] bg-cover bg-center">
+      <div
+        className="absolute inset-0 opacity-40"
+        style={{
+          backgroundImage: `url("data:image/svg+xml,%3Csvg viewBox='0 0 400 400' xmlns='http://www.w3.org/2000/svg'%3E%3Cfilter id='noiseFilter'%3E%3CfeTurbulence type='fractalNoise' baseFrequency='0.9' numOctaves='4' stitchTiles='stitch'/%3E%3C/filter%3E%3Crect width='100%25' height='100%25' filter='url(%23noiseFilter)'/%3E%3C/svg%3E")`,
+        }}
+      />
+      <div className="relative min-h-screen flex items-end">
+        <div className="p-8 pb-16">
+          <div>
+            <h1 className="text-5xl font-bold text-white leading-none tracking-tight">
+              Smarter Tournaments.
+              <br />
+              <span className="text-lime-400">Better Outcomes.</span>.
+            </h1>
+            <p className="text-xl text-gray-300 mt-4 font-light max-w-lg">
+              Streamline your esports events with next-gen tournament tools.
+            </p>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
-export default page;
+const Home = () => {
+  return (
+    <>
+      <Hero />
+    </>
+  );
+};
+
+export default Home;
