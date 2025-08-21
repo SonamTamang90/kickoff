8453176a4187fa0efb60f943c18e7fff19caf103
stamang <stamang.mrbhutan90@gmail.com>
2025-08-21 22:07:06 +0800

    feat: add sign out button for dashboard page

--- app/(dashboard)/dashboard/page.tsx
+++ app/(dashboard)/dashboard/page.tsx
@@ -1,8 +1,12 @@
 "use client";
 import SidebarLayout from "@/components/dashboard/SidebarLayout";
 import { Sidebar } from "@/components/dashboard/Sidebar";
+import { useAuth } from "@/lib/auth";
+import { Button } from "@/components/ui/Button";
 
 const Dashboard = () => {
+  const { logout } = useAuth();
+
   return (
     <SidebarLayout
       navbar={<nav>Side nav links</nav>}
@@ -11,6 +15,12 @@ const Dashboard = () => {
       <h1 className="text-dark-200 uppercase font-heading tracking-wider font-bold">
         Side Bar
       </h1>
+      <Button
+        onClick={logout}
+        className="mt-8 uppercase font-bold cursor-pointer"
+      >
+        Sign Out
+      </Button>
     </SidebarLayout>
   );
 };
